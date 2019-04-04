2f9c7c455fd17661ff8c0e651e7ea4e1c62a2f83
poy <poy@google.com>
2019-04-04 16:47:55 -0600

    refactor list and apps tests
    This CL prepares the way for additions to the apps command and the
    listing functionality.
    
    Change-Id: I1e53ad64a24ef4f443dc183740483c5b60573add

--- pkg/kf/commands/apps/apps_test.go
+++ pkg/kf/commands/apps/apps_test.go
@@ -3,15 +3,15 @@ package apps
 import (
 	"bytes"
 	"errors"
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/GoogleCloudPlatform/kf/pkg/kf"
 	"github.com/GoogleCloudPlatform/kf/pkg/kf/commands/config"
 	"github.com/GoogleCloudPlatform/kf/pkg/kf/fake"
+	"github.com/GoogleCloudPlatform/kf/pkg/kf/internal/testutil"
 	"github.com/golang/mock/gomock"
 	serving "github.com/knative/serving/pkg/apis/serving/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestAppsCommand(t *testing.T) {
@@ -19,47 +19,57 @@ func TestAppsCommand(t *testing.T) {
 	for tn, tc := range map[string]struct {
 		namespace string
 		wantErr   error
-		listErr   error
-		apps      []string
 		args      []string
+		setup     func(t *testing.T, fakeLister *fake.FakeLister)
+		assert    func(t *testing.T, buffer *bytes.Buffer)
 	}{
 		"invalid number of args": {
 			args:    []string{"invalid"},
 			wantErr: errors.New("accepts 0 arg(s), received 1"),
 		},
 		"configured namespace": {
-			namespace: "somenamespace",
+			namespace: "some-namespace",
+			setup: func(t *testing.T, fakeLister *fake.FakeLister) {
+				fakeLister.
+					EXPECT().
+					List(gomock.Any()).
+					Do(func(opts ...kf.ListOption) {
+						testutil.AssertEqual(t, "namespace", "some-namespace", kf.ListOptions(opts).Namespace())
+					})
+			},
 		},
 		"formats multiple services": {
-			apps: []string{"service-a", "service-b"},
+			setup: func(t *testing.T, fakeLister *fake.FakeLister) {
+				fakeLister.
+					EXPECT().
+					List(gomock.Any()).
+					Return([]serving.Service{
+						{ObjectMeta: metav1.ObjectMeta{Name: "service-a"}},
+						{ObjectMeta: metav1.ObjectMeta{Name: "service-b"}},
+					}, nil)
+			},
+			assert: func(t *testing.T, buffer *bytes.Buffer) {
+				header := "Getting apps in namespace: "
+				testutil.AssertContainsAll(t, buffer.String(), []string{header, "service-a", "service-b"})
+			},
 		},
 		"list applications error, returns error": {
-			listErr: errors.New("some-error"),
 			wantErr: errors.New("some-error"),
+			setup: func(t *testing.T, fakeLister *fake.FakeLister) {
+				fakeLister.
+					EXPECT().
+					List(gomock.Any()).
+					Return(nil, errors.New("some-error"))
+			},
 		},
 	} {
 		t.Run(tn, func(t *testing.T) {
 			ctrl := gomock.NewController(t)
-			defer ctrl.Finish()
 			fakeLister := fake.NewFakeLister(ctrl)
 
-			fakeRecorder := fakeLister.
-				EXPECT().
-				List(gomock.Any()).
-				DoAndReturn(func(opts ...kf.ListOption) ([]serving.Service, error) {
-					t.Helper()
-					if namespace := kf.ListOptions(opts).Namespace(); namespace != tc.namespace {
-						t.Fatalf("expected namespace %s, got %s", tc.namespace, namespace)
-					}
-
-					var apps []serving.Service
-					for _, a := range tc.apps {
-						s := serving.Service{}
-						s.Name = a
-						apps = append(apps, s)
-					}
-					return apps, tc.listErr
-				})
+			if tc.setup != nil {
+				tc.setup(t, fakeLister)
+			}
 
 			buffer := &bytes.Buffer{}
 
@@ -71,26 +81,15 @@ func TestAppsCommand(t *testing.T) {
 			c.SetArgs(tc.args)
 			gotErr := c.Execute()
 			if tc.wantErr != nil {
-				// We don't really care if Push was invoked if we want an
-				// error.
-				fakeRecorder.AnyTimes()
-
-				if fmt.Sprint(tc.wantErr) != fmt.Sprint(gotErr) {
-					t.Fatalf("wanted err: %v, got: %v", tc.wantErr, gotErr)
-				}
+				testutil.AssertErrorsEqual(t, tc.wantErr, gotErr)
 				return
 			}
 
-			header := fmt.Sprintf("Getting apps in namespace: %s", tc.namespace)
-			if strings.Index(buffer.String(), header) < 0 {
-				t.Fatalf("wanted header: %s: got:\n%v", header, buffer.String())
+			if tc.assert != nil {
+				tc.assert(t, buffer)
 			}
 
-			for _, app := range tc.apps {
-				if strings.Index(buffer.String(), app) < 0 {
-					t.Fatalf("wanted output: %s: got:\n%v", app, buffer.String())
-				}
-			}
+			ctrl.Finish()
 		})
 	}
 }
--- pkg/kf/list_test.go
+++ pkg/kf/list_test.go
@@ -15,44 +15,104 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestList(t *testing.T) {
+func TestLister_List(t *testing.T) {
 	t.Parallel()
 
+	setupListTest(
+		t,
+		func(s ...string) runtime.Object {
+			return createServiceList(s)
+		},
+		func(l *kf.Lister, opts ...kf.ListOption) ([]string, error) {
+			x, err := l.List(opts...)
+			if err != nil {
+				return nil, err
+			}
+
+			var names []string
+			for _, s := range x {
+				names = append(names, s.Name)
+			}
+			return names, nil
+		})
+}
+
+func setupListTest(t *testing.T, resultsF func(...string) runtime.Object, listF func(*kf.Lister, ...kf.ListOption) ([]string, error)) {
 	for tn, tc := range map[string]struct {
-		expectedNamespace string
-		appName           string
-		wantErr           error
 		servingFactoryErr error
-		serviceListErr    error
-		serviceNames      []string
-		opts              []kf.ListOption
+
+		reactor func(t *testing.T, action ktesting.Action) (handled bool, ret runtime.Object, err error)
+		do      func(t *testing.T, l *kf.Lister)
 	}{
 		"configured namespace": {
-			expectedNamespace: "some-namespace",
-			opts: []kf.ListOption{
-				kf.WithListNamespace("some-namespace"),
+			reactor: func(t *testing.T, action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+				testutil.AssertEqual(t, "namespace", "some-namespace", action.GetNamespace())
+
+				return false, nil, nil
+			},
+			do: func(t *testing.T, l *kf.Lister) {
+				listMustPass(t)(listF(l, kf.WithListNamespace("some-namespace")))
+			},
+		},
+		"default namespace": {
+			reactor: func(t *testing.T, action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+				testutil.AssertEqual(t, "namespace", "default", action.GetNamespace())
+
+				return false, nil, nil
+			},
+			do: func(t *testing.T, l *kf.Lister) {
+				listMustPass(t)(listF(l))
 			},
 		},
 		"configured app name": {
-			appName:           "some-app",
-			expectedNamespace: "default",
-			opts: []kf.ListOption{
-				kf.WithListAppName("some-app"),
+			reactor: func(t *testing.T, action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+				obj := action.(ktesting.ListActionImpl)
+
+				if len(obj.ListRestrictions.Fields.Requirements()) != 1 {
+					t.Fatalf("expected to have 1 requirement, got %d", len(obj.ListRestrictions.Fields.Requirements()))
+				}
+
+				testutil.AssertEqual(t, "FieldSelector Field", "metadata.name", obj.ListRestrictions.Fields.Requirements()[0].Field)
+				testutil.AssertEqual(t, "FieldSelector Value", "some-app", obj.ListRestrictions.Fields.Requirements()[0].Value)
+				return false, nil, nil
+			},
+			do: func(t *testing.T, l *kf.Lister) {
+				listMustPass(t)(listF(l, kf.WithListAppName("some-app")))
 			},
 		},
 		"formats multiple services": {
-			serviceNames:      []string{"service-a", "service-b"},
-			expectedNamespace: "default",
+			reactor: func(t *testing.T, action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+				testutil.AssertEqual(t, "namespace", "default", action.GetNamespace())
+				return true, resultsF("service-a", "service-b"), nil
+			},
+			do: func(t *testing.T, l *kf.Lister) {
+				expected := []string{"service-a", "service-b"}
+				actual := listMustPass(t)(listF(l))
+				for i, s := range expected {
+					testutil.AssertEqual(t, "name", s, actual[i])
+				}
+			},
 		},
 		"list services error, returns error": {
-			serviceListErr:    errors.New("some-error"),
-			wantErr:           errors.New("some-error"),
-			expectedNamespace: "default",
+			reactor: func(t *testing.T, action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+				testutil.AssertEqual(t, "namespace", "default", action.GetNamespace())
+
+				return true, nil, errors.New("some-error")
+			},
+			do: func(t *testing.T, l *kf.Lister) {
+				_, err := listF(l)
+				testutil.AssertErrorsEqual(t, errors.New("some-error"), err)
+			},
 		},
 		"serving factor error, returns error": {
 			servingFactoryErr: errors.New("some-error"),
-			wantErr:           errors.New("some-error"),
-			expectedNamespace: "default",
+			reactor: func(t *testing.T, action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+				return false, nil, nil
+			},
+			do: func(t *testing.T, l *kf.Lister) {
+				_, err := listF(l)
+				testutil.AssertErrorsEqual(t, errors.New("some-error"), err)
+			},
 		},
 	} {
 		t.Run(tn, func(t *testing.T) {
@@ -60,49 +120,46 @@ func TestList(t *testing.T) {
 				Fake: &ktesting.Fake{},
 			}
 
-			serviceList := &v1alpha1.ServiceList{}
-			for _, service := range tc.serviceNames {
-				serviceList.Items = append(serviceList.Items, v1alpha1.Service{
-					TypeMeta: metav1.TypeMeta{
-						Kind:       "service",
-						APIVersion: "serving.knative.dev/v1alpha1",
-					},
-					ObjectMeta: metav1.ObjectMeta{
-						Name: service,
-					},
-				})
-			}
-
 			fake.AddReactor("*", "*", ktesting.ReactionFunc(func(action ktesting.Action) (handled bool, ret runtime.Object, err error) {
-				obj := action.(ktesting.ListActionImpl)
-				testutil.AssertEqual(t, "namespace", tc.expectedNamespace, action.GetNamespace())
-
-				if tc.appName != "" {
-					if len(obj.ListRestrictions.Fields.Requirements()) != 1 {
-						t.Fatalf("expected to have 1 requirement, got %d", len(obj.ListRestrictions.Fields.Requirements()))
-					}
-
-					testutil.AssertEqual(t, "FieldSelector Field", "metadata.name", obj.ListRestrictions.Fields.Requirements()[0].Field)
-					testutil.AssertEqual(t, "FieldSelector Value", tc.appName, obj.ListRestrictions.Fields.Requirements()[0].Value)
+				if tc.reactor != nil {
+					return tc.reactor(t, action)
 				}
-
-				return true, serviceList, tc.serviceListErr
+				return false, nil, nil
 			}))
 
 			lister := kf.NewLister(func() (serving.ServingV1alpha1Interface, error) {
 				return fake, tc.servingFactoryErr
 			})
 
-			apps, gotErr := lister.List(tc.opts...)
-
-			if tc.wantErr != nil {
-				testutil.AssertErrorsEqual(t, tc.wantErr, gotErr)
-				return
-			}
-
-			for i, service := range tc.serviceNames {
-				testutil.AssertEqual(t, "app", service, apps[i].Name)
-			}
+			tc.do(t, lister)
 		})
 	}
+}
+
+func listMustPass(t *testing.T) func([]string, error) []string {
+	t.Helper()
+	return func(s []string, err error) []string {
+		t.Helper()
+		if err != nil {
+			t.Fatal(err)
+		}
+		return s
+	}
+}
+
+func createServiceList(names []string) *v1alpha1.ServiceList {
+	serviceList := &v1alpha1.ServiceList{}
+	for _, service := range names {
+		serviceList.Items = append(serviceList.Items, v1alpha1.Service{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "service",
+				APIVersion: "serving.knative.dev/v1alpha1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: service,
+			},
+		})
+	}
+
+	return serviceList
 }
