6e8f95a528a6b0a7a16dee24ea5d4e88df78e5ba
Matthew Broomfield <mattysweeps@google.com>
2019-06-11 17:04:09 -0700

    try

--- hack/concourse-login.sh
+++ hack/concourse-login.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -euo pipefail
+set -eu
 
 readonly target=${1:?Error: Please supply a target}
 
