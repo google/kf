bbbea3a695f46180d1b70ff0378c524921221aec
Matthew Broomfield <mattysweeps@google.com>
2019-06-21 15:46:29 -0700

    dont know how i moved this

--- pkg/apis/kf/v1alpha1/register.go
+++ pkg/apis/kf/v1alpha1/register.go
@@ -14,7 +14,45 @@
 
 package v1alpha1
 
-// GroupName is the group name used in this package
-const (
-	GroupName = "kf.dev"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
+
+var SchemeGroupVersion = schema.GroupVersion{
+	Group:   "kf.dev",
+	Version: "v1alpha1",
+}
+
+var (
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	// We only register manually written functions here. The registration of the
+	// generated functions takes place in the generated files. The separation
+	// makes the code compile even when the generated files are missing.
+	localSchemeBuilder.Register(addKnownTypes)
+}
+
+// Resource takes an unqualified resource and returns a Group qualified GroupResource
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
+}
+
+// Adds the list of known types to the given scheme.
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
+		&App{},
+		&Source{},
+		&Space{},
+		&metav1.Status{},
+	)
+
+	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
+	return nil
+}
