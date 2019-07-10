f4f9dfee53903bf97b675b70d424520583b20ca1
Andrew Poydence <poy@google.com>
2019-07-10 09:29:50 -0600

    hack: adds hack/check-go-sum.sh to ensure go.sum is correct (#278)
    fixes #263

--- hack/check-go-sum.sh
+++ hack/check-go-sum.sh
@@ -14,22 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script is used by the CI to check if the code is gofmt formatted.
-# This script is used by the CI to check if 'go generate ./...' is up to date.
+# This script is used by the CI to check if go.sum is valid.
 
-set -eu
+cd $(dirname $(go env GOMOD))
 
-go install github.com/google/wire/cmd/wire
-go install github.com/golang/mock/mockgen
-export PATH="$PATH:$(go env GOPATH)/bin"
-go generate ./...
 go mod tidy
 
 if [ ! -z "$(git status --porcelain)" ]; then
     git status
     echo
-    echo "The generated files aren't up to date."
-    echo "Update them with the 'go generate ./...' command."
+    echo "go.sum isn't correct."
     git --no-pager diff
     exit 1
 fi
