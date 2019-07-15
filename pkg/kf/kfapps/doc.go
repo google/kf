231dd0a6a4aa688b4d12a51c6097d8579a8fdf07
Andrew Poydence <poy@google.com>
2019-07-15 10:23:35 -0600

    pkg/kf/commands/apps: adds scale command (#308)
    * pkg/kf/commands/apps: adds scale command
    
    This only implements instance scaling (and not CPU or memory). It allows
    for exact scaling (via `--instances` flag) and autoscaler settings (via
    `--autoscale-min` and `--autoscale-max` flags).
    
    fixes #285
    
    * change autoscaler flag names to min and max
    
    * let spec do validation and pretty printing

---
+++ pkg/kf/kfapps/doc.go
@@ -0,0 +1,20 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package apps provides a cf compatible way of managing Knative Services as
+// cf apps.
+package kfapps
+
+//go:generate go run ../internal/tools/option-builder/option-builder.go --pkg kfapps ../internal/tools/clientgen/common-options.yml zz_generated.clientoptions.go
+//go:generate go run ../internal/tools/clientgen/genclient.go client.yml
