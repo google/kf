e8610b72047e604985f0fb8a88487ba4c699010d
Andrew Poydence <poy@google.com>
2019-07-18 06:56:03 -0600

    pkg/kf/commands: adds version command (#343)
    version will display the current version. The version is provided via
    ldflags. The hack/upload-release.sh script (used by the nightly build)
    has been updated to include the current hash.
    
    fixes #320

---
+++ pkg/kf/commands/version.go
@@ -0,0 +1,38 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package commands
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// NewVersionCommand returns a command that displays the version.
+func NewVersionCommand(version, goos string) *cobra.Command {
+	return &cobra.Command{
+		Use:     "version",
+		Short:   "Display the CLI version",
+		Example: `  kf version`,
+		Args:    cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Fprintln(cmd.OutOrStdout(), "kf version", version, goos)
+			return nil
+		},
+	}
+}
+
+// Version is filled in via ldflags.
+var Version = "dev"
