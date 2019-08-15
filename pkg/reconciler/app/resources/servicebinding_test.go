be38dd3f49d04c0d28919ae2adbf4bbc9e5ec286
Matthew Broomfield <mattysweeps@google.com>
2019-08-15 09:29:22 -0700

    add a few tests

--- pkg/reconciler/app/resources/servicebinding_test.go
+++ pkg/reconciler/app/resources/servicebinding_test.go
@@ -15,8 +15,46 @@
 package resources
 
 import (
-	"testing"
+	"fmt"
+
+	"github.com/google/kf/pkg/apis/kf/v1alpha1"
+	servicecatalogv1beta1 "github.com/poy/service-catalog/pkg/apis/servicecatalog/v1beta1"
 )
 
-func TestXxx(t *testing.T) {
+func ExampleMakeServiceBindingLabels() {
+	app := &v1alpha1.App{}
+	app.Name = "my-app"
+	binding := &v1alpha1.AppSpecServiceBinding{
+		InstanceRef: servicecatalogv1beta1.LocalObjectReference{
+			Name: "my-service",
+		},
+		BindingName: "cool-binding",
+	}
+	app.Spec.ServiceBindings = []v1alpha1.AppSpecServiceBinding{*binding}
+
+	labels := MakeServiceBindingLabels(app, binding.BindingName)
+	for k, v := range labels {
+		fmt.Println(k, ":", v)
+	}
+
+	// Output: app.kubernetes.io/managed-by : kf
+	// app.kubernetes.io/name : my-app
+	// app.kubernetes.io/component : servicebinding
+	// kf-app-name : my-app
+	// kf-binding-name : cool-binding
+}
+
+func ExampleMakeServiceBindingName() {
+	app := &v1alpha1.App{}
+	app.Name = "my-app"
+	binding := &v1alpha1.AppSpecServiceBinding{
+		InstanceRef: servicecatalogv1beta1.LocalObjectReference{
+			Name: "my-service",
+		},
+	}
+	app.Spec.ServiceBindings = []v1alpha1.AppSpecServiceBinding{*binding}
+
+	fmt.Println(MakeServiceBindingName(app, binding))
+
+	// Output: kf-binding-my-app-my-service
 }
