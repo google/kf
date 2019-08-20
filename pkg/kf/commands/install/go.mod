ef3df58a74c5f22cfa08519bfc9b0abae7bd733c
poy <poy@google.com>
2019-08-19 19:45:29 -0600

    pkg/kf/commands/install: refactor to use ExponetialBackoff
    Replace hand written backoff code in favor of library.

--- pkg/kf/commands/install/go.mod
+++ pkg/kf/commands/install/go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/fatih/color v1.7.0
 	github.com/manifoldco/promptui v0.3.2
 	github.com/spf13/cobra v0.0.5
+	k8s.io/apimachinery v0.0.0-20190814100815-533d101be9a6
 )
--- pkg/kf/commands/install/kf/kf.go
+++ pkg/kf/commands/install/kf/kf.go
@@ -22,6 +22,7 @@ import (
 	"time"
 
 	. "github.com/google/kf/pkg/kf/commands/install/util"
+	"k8s.io/apimachinery/pkg/util/wait"
 )
 
 const (
@@ -49,28 +50,26 @@ func Install(ctx context.Context, containerRegistry string) error {
 		{name: "Knative Build", yaml: KnativeBuildYAML},
 		{name: "kf", yaml: KfNightlyBuildYAML},
 	} {
-
-		var err error
-		for i := 0; i < 10; i++ {
+		wait.ExponentialBackoff(wait.Backoff{
+			Duration: time.Second,
+			Steps:    10,
+			Factor:   1,
+		}, func() (bool, error) {
 			Logf(ctx, "install "+yaml.name)
-			if _, err = Kubectl(
+			if _, err := Kubectl(
 				ctx,
 				"apply",
 				"--filename",
 				yaml.yaml,
 			); err != nil {
-				Logf(ctx, "failed to install %s... Retrying %d/10", yaml.name, i+1)
-				time.Sleep(time.Second)
-				continue
+				Logf(ctx, "failed to install %s... Retrying", yaml.name)
+				// Don't return the error. This will cause the
+				// ExponentialBackoff to stop.
+				return false, nil
 			}
 
-			break
-		}
-
-		if err != nil {
-			// Looks like the retrying didn't help, better just return the error
-			return err
-		}
+			return true, nil
+		})
 	}
 
 	// Setup kf space
