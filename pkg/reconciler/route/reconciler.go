2c231b7f2d7ea7c017f399ec8f50cac787c049ab
Julia Guo <juliaguo@google.com>
2019-10-08 16:56:07 -0700

    undo reconciler accidental change

--- pkg/reconciler/route/reconciler.go
+++ pkg/reconciler/route/reconciler.go
@@ -87,7 +87,7 @@ func (r *Reconciler) ApplyChanges(
 	// Fetch Claims with the same Hostname+Domain.
 	claims, err := r.routeClaimLister.
 		RouteClaims(namespace).
-		List(appresources.MakeRouteSelector(fields))
+		List(appresources.MakeRouteSelectorNoPath(fields))
 	if err != nil {
 		return err
 	}
@@ -127,7 +127,7 @@ func (r *Reconciler) ApplyChanges(
 	// Fetch routes with the same Hostname+Domain.
 	routes, err := r.routeLister.
 		Routes(namespace).
-		List(appresources.MakeRouteSelector(fields))
+		List(appresources.MakeRouteSelectorNoPath(fields))
 	if err != nil {
 		return err
 	}
