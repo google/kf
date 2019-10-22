fde75b16e178e34fba7f7320046f55a548885531
Joseph Lewis III <jlewisiii@google.com>
2019-10-22 14:55:26 -0700

    initial work

--- pkg/kf/testutil/integration.go
+++ pkg/kf/testutil/integration.go
@@ -33,6 +33,16 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"knative.dev/pkg/injection"
 )
 
 const (
@@ -91,6 +101,123 @@ func RunIntegrationTest(t *testing.T, test func(ctx context.Context, t *testing.
 	t.Log()
 
 	test(ctx, t)
+}
+
+// RunKubeAPITest is for executing tests against the Kubernetes API directly.
+func RunKubeAPITest(t *testing.T, test func(ctx context.Context, t *testing.T)) {
+	if !strings.HasPrefix(t.Name(), "TestIntegration_") {
+		// We want to enforce a convention so scripts can single out
+		// integration tests.
+		t.Fatalf("Integration tests must have the name format of 'TestIntegration_XXX`")
+	}
+
+	t.Helper()
+	if testing.Short() {
+		t.Skip()
+	}
+
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	clientCfg := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, &clientcmd.ConfigOverrides{})
+	restCfg, err := clientCfg.ClientConfig()
+	if err != nil {
+		t.Fatalf("couldn't fetch kubeconfig: %v", err)
+	}
+
+	start := time.Now()
+	defer func() {
+		state := "PASSED"
+		if t.Failed() {
+			state = "FAILED"
+		}
+		t.Logf("Test %s took %v and %s", t.Name(), time.Since(start), state)
+	}()
+
+	// Setup context that will allow us to cleanup if the user wants to
+	// cancel the tests.
+	ctx, cancel := context.WithCancel(context.Background())
+
+	ctx = contextWithRestConfig(ctx, restCfg)
+
+	// Set up the autowired informers so they can be used in testing
+	ctx, _ = injection.Default.SetupInformers(ctx, restCfg)
+
+	// Give everything time to clean up.
+	defer time.Sleep(time.Second)
+	defer cancel()
+	CancelOnSignal(ctx, cancel, t.Log)
+	t.Log()
+
+	test(ctx, t)
+}
+
+type restConfigKey struct{}
+
+func contextWithRestConfig(ctx context.Context, cfg *rest.Config) context.Context {
+	return context.WithValue(ctx, restConfigKey{}, cfg)
+}
+
+func restConfigFromContext(ctx context.Context) *rest.Config {
+	return ctx.Value(restConfigKey{}).(*rest.Config)
+}
+
+// WithRestConfig gets the rest config from the context and passes it to the callback.
+func WithRestConfig(ctx context.Context, t *testing.T, callback func(cfg *rest.Config)) {
+	t.Helper()
+
+	callback(restConfigFromContext(ctx))
+}
+
+// WithKubernetes creates a Kubernetes client from the config on the context
+// and passes it to the callback.
+func WithKubernetes(ctx context.Context, t *testing.T, callback func(k8s kubernetes.Interface)) {
+	t.Helper()
+
+	WithRestConfig(ctx, t, func(cfg *rest.Config) {
+		k8s, err := kubernetes.NewForConfig(cfg)
+		if err != nil {
+			t.Fatalf("Creating Kubernetes: %v", err)
+		}
+
+		callback(k8s)
+	})
+}
+
+// WithDynamicClient creates a dynamic Kubernetes client from the config on the
+// context and passes it to the callback.
+func WithDynamicClient(ctx context.Context, t *testing.T, callback func(client dynamic.Interface)) {
+	t.Helper()
+
+	WithRestConfig(ctx, t, func(cfg *rest.Config) {
+		dynamic, err := dynamic.NewForConfig(cfg)
+		if err != nil {
+			t.Fatalf("Creating dynamic client: %v", err)
+		}
+
+		callback(dynamic)
+	})
+}
+
+// WithNamespace creates a namespace and deletes it after the test is done.
+// It can be used within the context of a RunKubeAPITest.
+func WithNamespace(ctx context.Context, t *testing.T, callback func(namespace string)) {
+	t.Helper()
+
+	WithKubernetes(ctx, t, func(k8s kubernetes.Interface) {
+		name := fmt.Sprintf("integration-%d", time.Now().UnixNano())
+		namespace := &corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: name,
+			},
+		}
+
+		if _, err := k8s.CoreV1().Namespaces().Create(namespace); err != nil {
+			t.Fatalf("Creating namespace: %v", err)
+		}
+		defer k8s.CoreV1().Namespaces().Delete(name, &metav1.DeleteOptions{})
+
+		t.Logf("With Namespace: %s", name)
+		callback(name)
+	})
 }
 
 // CancelOnSignal watches for a kill signal. If it gets one, it invokes the
---
+++ pkg/reconciler/integration/defaults_test.go
@@ -0,0 +1,80 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/google/kf/pkg/kf/testutil"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
+	"sigs.k8s.io/yaml"
+)
+
+func TestIntegration_ApplyDefaults(t *testing.T) {
+	cases := map[string]struct {
+		Group    string
+		Version  string
+		Resource string
+		File     string
+	}{
+		"serving": {
+			Group:    "serving.knative.dev",
+			Version:  "v1alpha1",
+			Resource: "services",
+			File:     "serving_v1alpha1_defaults.yaml",
+		},
+	}
+
+	for tn, tc := range cases {
+		t.Run(tn, func(t *testing.T) {
+			testutil.RunKubeAPITest(t, func(ctx context.Context, t *testing.T) {
+				testutil.WithNamespace(ctx, t, func(namespace string) {
+					testutil.WithDynamicClient(ctx, t, func(client dynamic.Interface) {
+						// pass
+						dyn := client.Resource(schema.GroupVersionResource{
+							Group:    tc.Group,
+							Version:  tc.Version,
+							Resource: tc.Resource,
+						})
+
+						objPath := filepath.Join("testdata", tc.File)
+						objBytes, err := ioutil.ReadFile(objPath)
+						testutil.AssertNil(t, "read error", err)
+
+						unstructuredContent := make(map[string]interface{})
+						err = yaml.Unmarshal(objBytes, &unstructuredContent)
+						testutil.AssertNil(t, "unmarshal error", err)
+
+						obj := &unstructured.Unstructured{}
+						obj.SetUnstructuredContent(unstructuredContent)
+						obj.SetNamespace(namespace)
+
+						out, err := dyn.Create(obj, metav1.CreateOptions{})
+						testutil.AssertNil(t, "creation error", err)
+						fmt.Println(out.UnstructuredContent())
+					})
+				})
+			})
+		})
+	}
+
+}
