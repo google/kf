6d01a81b09a578a3ec70f1b2ae29849cf7a94abf
poy <poy@google.com>
2020-11-16 14:13:59 -0700

    Adds dashboard template and script.

---
+++ dashboards/README.md
@@ -0,0 +1,35 @@
+# Kf Dashboards
+
+### Script
+
+The [create-dashboard.py](create-dashboard.py) script is a python script that
+will use the [dashboard-template.json](dashboard-template.json) file to create
+a template. It uses the currently targeted GCP project (e.g., `gcloud config
+get-value project`).
+
+It takes the following 3 arguments:
+
+1. Dashboard name - The name of the dashboard.
+1. Cluster name - The name of the Kubernetes cluster.
+1. Space - The name of the Kf space.
+
+Note: Running the script is not idempotent.
+
+Example:
+
+```
+./create-dashboard.py my-dashboard my-cluster my-space
+```
+
+### Templates
+
+The JSON templates are created using steps found in the [Cloud Monitoring
+Dashboard Samples](https://github.com/GoogleCloudPlatform/monitoring-dashboard-samples/blob/master/README.md).
+
+They each contain the following sentinel values:
+* `XXX-DASHBOARD-XXX` - The name of the dashboard.
+* `XXX-CLUSTER-XXX` - The name of the Kubernetes cluster.
+* `XXX-SPACE-XXX` - The name of the Kf Space.
+
+If you plan to use this template directly (instead of using the script), be
+sure to replace each instance of each sentinel value.
---
+++ dashboards/create-dashboard.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+import os
+import subprocess
+import sys
+
+
+def build_template(dashboard_name, cluster_name, space):
+    # Find the template directory. We assume it is in the same location as the
+    # script. We assume it is in the same location as the script. We assume it
+    # is in the same location as the script. We assume it is in the same
+    # location as the script.
+    dashboard_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "dashboard-template.json")
+
+    # Replace the sentinel values.
+    with open(dashboard_path) as f:
+        return f \
+            .read() \
+            .replace("XXX-DASHBOARD-XXX", dashboard_name) \
+            .replace("XXX-CLUSTER-XXX", cluster_name) \
+            .replace("XXX-SPACE-XXX", space)
+
+
+def execute(command):
+    call = subprocess.run(command, stdout=subprocess.PIPE, input=b'y', check=True)
+    return call.stdout.decode("utf-8")
+
+
+def extract_dashboard_id(dashboard_object):
+    # example dashboard name: projects/someprojectid/dashboards/dashboard-id
+    return dashboard_object["name"].split('/')[3]
+
+
+def create_dashboard_url(dashboard_id):
+    return "https://console.cloud.google.com/monitoring/dashboards/custom/" + dashboard_id
+
+
+def create_dashboard(dashboard_name, cluster_name, space):
+    config = build_template(dashboard_name, cluster_name, space)
+    output = execute(["gcloud", "monitoring", "dashboards", "create", "--format", "json", "--config", config])
+    return create_dashboard_url(extract_dashboard_id(json.loads(output)))
+
+
+def main():
+    # Ensure we have enough args
+    if len(sys.argv) != 4:
+        proc_name = sys.argv[0]
+        print("Usage: {proc_name} [DASHBOARD NAME] [CLUSTER NAME] [SPACE]")
+        sys.exit(1)
+
+    dashboard_name  = sys.argv[1]
+    cluster_name  = sys.argv[2]
+    space  = sys.argv[3]
+
+    print(create_dashboard(dashboard_name, cluster_name, space))
+
+
+if __name__ == "__main__":
+    main()
