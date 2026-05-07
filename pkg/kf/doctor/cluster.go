8ddad6a9f80695a49300b96ee469f5319a56eb55
Jakub Węgrzyn <jakweg@google.com>
2026-05-07 12:25:00 +0000

    Generalize condition ignorance

--- pkg/kf/doctor/cluster.go
+++ pkg/kf/doctor/cluster.go
@@ -422,6 +422,11 @@ func diagnoseDaemonSets(ctx context.Context, d *Diagnostic, kubernetes kubernete
 	}
 }
 
+func conditionShouldBeIgnored(condition corev1.NodeCondition) bool {
+	shouldIgnore := condition.Type == "SysctlChanged" && condition.Status != corev1.ConditionFalse && condition.Message == "{\"unmanaged\": {\"kernel.cad_pid\": \"1\"}}"
+	return shouldIgnore
+}
+
 func diagnoseNodes(ctx context.Context, d *Diagnostic, kubernetes kubernetes.Interface) {
 	nodesList, err := kubernetes.CoreV1().Nodes().List(ctx, metav1.ListOptions{})
 	switch {
@@ -500,21 +505,10 @@ func diagnoseNodes(ctx context.Context, d *Diagnostic, kubernetes kubernetes.Int
 									cond.Message,
 								)
 							}
-						case cond.Type == "SysctlChanged":
-							if cond.Status != corev1.ConditionFalse {
-								if cond.Message != "{\"unmanaged\": {\"kernel.cad_pid\": \"1\"}}" {
-									d.Errorf(
-										"Condition %s is not healthy, current status is %q with message %q",
-										cond.Type,
-										cond.Status,
-										cond.Message,
-									)
-								}
-							}
 						default:
-							// Other conditions on the node should be false because they
-							// indicate specific failure conditions when set to true.
-							if cond.Status != corev1.ConditionFalse {
+							// Other conditions on the node should be false (unless known issue)
+							// because they indicate specific failure conditions when set to true.
+							if cond.Status != corev1.ConditionFalse && !conditionShouldBeIgnored(cond) {
 								d.Errorf(
 									"Condition %s is not healthy, current status is %q with message %q",
 									cond.Type,
